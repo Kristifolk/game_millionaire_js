cf15f27ede30b1e4f08f4006fe9d417744e17f15
Kristifolk <kristin.kar13@gmail.com>
2023-05-09 14:22:23 +0300

    refactoring code

--- js/script.js
+++ js/script.js
@@ -1,82 +1,61 @@
-
-
 var event, ok;
-
 var answers = [];
 var question, welcome, sum ;
 var count = 0;
 sum = 0;
-console.log('2');
 welcome = +prompt('Игра "Кто хочет стать миллионером"\n' + 'За каждый правильный ответ вы получите 250000р. В случае хоть одного ошибочного ответа - вся накопленная сумма сгорит =( \n' +
 'Удачи! Хотите начать игру? Нажмите 1 или -1 - Выход из игры');
-    if(welcome == 1){console.log('3');
+    if(welcome == 1){
         askQuestions(questions);
-    } else if(welcome == -1) { console.log('4');
+    } else if(welcome == -1) {
         alert('Жаль, игра не успела даже начаться');
     }
-    else { console.log('5');
+    else {
         askAgain();
     }
 
-
 function askQuestions(array) { 
-    console.log('7'); 
-    console.log(array);
-    
     for(var i = count; i<array.length;){
-        console.log('8');
         event = +prompt(array[i].q + array[i].a1 + array[i].a2 + array[i].a3 + array[i].a4);
         var result = isAnswer(questions[i],event);
-        console.log(result);
         if(false != result) {
-                
             if(event == array[i].correct) { 
-                console.log('9');
-                    sum += array[i].money;
-                    count += 1;
-                    i += 1;
-                    if(count == 4){console.log('100');
-                        alert('Поздравляем, вы правильно ответили! И выиграли 250 000!\n' + 'Всего ваш выигрыш составляет ' + sum +
-                        '\nУра,вы миллионер!');
-                    } else{
-                        event = +prompt ('Верно и вы выиграли 250 000!\n' + 'Всего ваш выигрыш составляет ' + sum +
-                        '\nХотите продолжить игру? Нажмите 1 или -1 - Выход из игры с выигрышем  ' + sum )
-                        switch (event) { 
-                            case 1: 
-                            console.log('10');
-                                break;
-                                
-                            case -1:
-                                console.log('11');
-                                alert('Как жаль хотелось поиграть еще. Ваш выигрыш составляет ' + sum +' Поздравляем!');
-                                return false;
-                                
-                            default:
-                                console.log('12');
-                                askAgain();
-                                return false;
-                                
-                        }
+                sum += array[i].money;
+                count += 1;
+                i += 1;
+                if(count == 4){
+                    alert('Поздравляем, вы правильно ответили! И выиграли 250 000!\n' + 'Всего ваш выигрыш составляет ' + sum +
+                    '\nУра,вы миллионер!');
+                } else{
+                    event = +prompt ('Верно и вы выиграли 250 000!\n' + 'Всего ваш выигрыш составляет ' + sum +
+                    '\nХотите продолжить игру? Нажмите 1 или -1 - Выход из игры с выигрышем  ' + sum )
+                    switch (event) { 
+                        case 1: 
+                            break;
+                        case -1:
+                            alert('Как жаль хотелось поиграть еще. Ваш выигрыш составляет ' + sum +' Поздравляем!');
+                            return false;
+                        default:
+                            askAgain();
+                            return false;
                     }
+                }
             } else if(event != array[i].correct){ 
-                console.log('13');
-                    alert('Увы, ответ ошибочный, вы проиграли');
+                alert('Увы, ответ ошибочный, вы проиграли');
                     break;
-            }  else { console.log('133');
+            }  else { 
                     isAnswer(questions[i],event)
             }// проверка на ввод цифр 5,10, 15 и тд
         } 
     } 
 }
 
-function askAgain(){ console.log('askAgain')
+function askAgain(){ 
     a = +prompt('Принимается ответ только 1 или -1');
     if(a == 1){
-        console.log('1');
-        console.log(questions);
         askQuestions(questions);
-    }else if(a == -1){ console.log('-1');
-            alert('Как жаль хотелось поиграть еще. Ваш выигрыш составляет ' + sum +' Поздравляем!');
+    }else if(a == -1){ 
+        alert('Как жаль хотелось поиграть еще. Ваш выигрыш составляет ' + sum +' Поздравляем!');
         return false;
     }else {
         askAgain();     
@@ -94,6 +73,5 @@ function isAnswer(a0, event) {
         return false;
     }
 	return true;
-    
 }
 
